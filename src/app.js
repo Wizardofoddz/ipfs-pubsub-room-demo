e593671f4647b412ec6229dca7df24509796dedd
Luis Montes <luis@iceddev.com>
2018-02-21 17:04:52 -0700

    fix build and change room

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const IPFS = require('ipfs')
 const Room = require('ipfs-pubsub-room')
+const me = 'me' + Math.random();
+
+console.log('launching...', me);
 
 const ipfs = new IPFS({
   repo: repo(),
@@ -15,25 +18,24 @@ const ipfs = new IPFS({
   }
 })
 
+
 ipfs.once('ready', () => ipfs.id((err, info) => {
   if (err) { throw err }
   console.log('IPFS node ready with address ' + info.id)
 
-  const room = Room(ipfs, 'ipfs-pubsub-demo')
+  const room = Room(ipfs, 'ipfs-pubsub-demo2')
 
   room.on('peer joined', (peer) => console.log('peer ' + peer + ' joined'))
   room.on('peer left', (peer) => console.log('peer ' + peer + ' left'))
 
-  // send and receive messages
+  room.on('message', (message) => {
+    console.log('got message from ' + message.from + ': ' + message.data.toString(), message);
+  })
 
-  room.on('peer joined', (peer) => room.sendTo(peer, 'Hello ' + peer + '!'))
-  room.on('message', (message) => console.log('got message from ' + message.from + ': ' + message.data.toString()))
-
-  // broadcast message every 2 seconds
-
-  setInterval(() => room.broadcast('hey everyone!'), 2000)
-}))
+  window.room = room;
+  window.ipfs = ipfs;
+}));
 
 function repo () {
-  return 'ipfs/pubsub-demo/' + Math.random()
+  return 'ipfs/pubsub-demo2/' + me;
 }
